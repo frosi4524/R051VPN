95a6c08493e4af51bc8ed2b908c62f41fdeeee97
frosi4524 <frosi4524@gmail.com>
2025-04-05 09:51:41 +0700

    Update installsl.sh

--- SKT-DnsTT/installsl.sh
+++ SKT-DnsTT/installsl.sh
@@ -67,15 +67,15 @@ ns_domain_cloudflare() {
 setup_dnstt() {
 	cd
 	mkdir -p /etc/slowdns
-	wget -O dnstt-server "https://raw.githubusercontent.com/rosi606/R051VPN/main/SKT-DnsTT/dnstt-server" >/dev/null 2>&1
+	wget -O dnstt-server "https://raw.githubusercontent.com/frosi4524/R051VPN/main/SKT-DnsTT/dnstt-server" >/dev/null 2>&1
 	chmod +x dnstt-server >/dev/null 2>&1
-	wget -O dnstt-client "https://raw.githubusercontent.com/rosi606/R051VPN/main/SKT-DnsTT/dnstt-client" >/dev/null 2>&1
+	wget -O dnstt-client "https://raw.githubusercontent.com/frosi4524/R051VPN/main/SKT-DnsTT/dnstt-client" >/dev/null 2>&1
 	chmod +x dnstt-client >/dev/null 2>&1
 	./dnstt-server -gen-key -privkey-file server.key -pubkey-file server.pub
 	chmod +x *
 	mv * /etc/slowdns
-	wget -O /etc/systemd/system/client.service "https://raw.githubusercontent.com/rosi606/R051VPN/main/SKT-DnsTT/client" >/dev/null 2>&1
-	wget -O /etc/systemd/system/server.service "https://raw.githubusercontent.com/rosi606/R051VPN/main/SKT-DnsTT/server" >/dev/null 2>&1
+	wget -O /etc/systemd/system/client.service "https://raw.githubusercontent.com/frosi4524/R051VPN/main/SKT-DnsTT/client" >/dev/null 2>&1
+	wget -O /etc/systemd/system/server.service "https://raw.githubusercontent.com/frosi4524/R051VPN/main/SKT-DnsTT/server" >/dev/null 2>&1
 	sed -i "s/xxxx/$NS_DOMAIN/g" /etc/systemd/system/client.service 
 	sed -i "s/xxxx/$NS_DOMAIN/g" /etc/systemd/system/server.service 
 }
