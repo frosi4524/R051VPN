23734374108006edb5d69525f304d2210e1d2e76
frosi4524 <frosi4524@gmail.com>
2025-04-05 09:49:37 +0700

    Update vpn.sh

--- SKT-System/vpn.sh
+++ SKT-System/vpn.sh
@@ -4,7 +4,7 @@
 # By SKT AIO
 # ==================================================
 # Link Hosting Kalian
-SKAI0="raw.githubusercontent.com/rosi606/R051VPN/main/SKT-System/"
+SKAI0="raw.githubusercontent.com/frosi4524/R051VPN/main/SKT-System/"
 
 # initialisasi var
 export DEBIAN_FRONTEND=noninteractive
@@ -18,7 +18,7 @@ apt install openvpn easy-rsa unzip -y
 apt install openssl iptables iptables-persistent -y
 mkdir -p /etc/openvpn/server/easy-rsa/
 cd /etc/openvpn/
-wget https://raw.githubusercontent.com/rosi606/R051VPN/main/SKT-System/vpn.zip
+wget https://raw.githubusercontent.com/frosi4524/R051VPN/main/SKT-System/vpn.zip
 unzip vpn.zip
 rm -f vpn.zip
 chown -R root:root /etc/openvpn/server/easy-rsa/
