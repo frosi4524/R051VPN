4c4d5290ebd58bc3c086cf0f2d517c4e22f55bb3
frosi4524 <frosi4524@gmail.com>
2025-04-05 08:55:47 +0700

    Update m-trojan.sh

--- menu/m-trojan.sh
+++ menu/m-trojan.sh
@@ -44,7 +44,7 @@ check_ip_and_get_info() {
 # =============================================
 
 # Ambil data dari GitHub dengan timeout
-permission_file=$(curl -s --connect-timeout 10 https://raw.githubusercontent.com/rosi606/1z1n/main/L1cense.txt)
+permission_file=$(curl -s --connect-timeout 10 https://raw.githubusercontent.com/frosi4524/1z1n/main/L1cense.txt)
 
 # Validasi file permission
 if [ -z "$permission_file" ]; then
